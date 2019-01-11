029c013eb1ac4da4e5e14e5ac043b9e2e1b20f3c
Ummer Khan <ummernkhangit@gmail.com>
2019-01-11 14:12:45 -0800

    committed

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ function init() {
 function render() {
     // if (parseInt(output.innerHTML.charAt(0)) == '-') {
     //     output.style.color = 'red';
-    }
+    // }
     console.log('render');
 }
 
